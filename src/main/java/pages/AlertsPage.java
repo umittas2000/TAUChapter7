734a88b57f84e1af1492986091b5200f4a64cbfa
Umit Tas <umittas2000@gmail.com>
2024-10-20 00:24:45 -0300

    Week15 initial commit

---
+++ src/main/java/pages/AlertsPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AlertsPage {
+    private WebDriver driver;
+
+    //Constructor
+    public AlertsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Locators
+
+
+    //Action Methods
+
+}
